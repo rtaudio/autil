2338d58387e40a064d1692cecdc7884bdbae239f
Fabian Schlieper <fabian@schlieper.email>
2016-11-15 23:27:26 +0100

    added test.h

---
+++ test.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <cstdint>
+
+
+namespace autil {
+
+class test
+{
+public:
+	static inline uint32_t fastRand(void)
+	{
+		if (!Rz && !Rw) fastRandInit();
+		Rz = 36969 * (Rz & 65535) + (Rz >> 16);
+		Rw = 18000 * (Rw & 65535) + (Rw >> 16);
+		return (Rz << 16) + Rw;
+	}
+
+	static void fastRandInit();
+
+	static void generateMusic(float *buf, int len, bool inititalSilence = false);
+	static void generateNoise(float *buf, int len);
+
+private:
+	static uint32_t Rz, Rw;
+	
+	test() {};
+};
+}
